99459a982d6c0858d10a1336b701c12ddc968099
moh raihan romadhoni <126458829+raihan-doni16@users.noreply.github.com>
2024-06-03 09:33:17 +0700

    add img

--- resources/views/Front/footer.blade.php
+++ resources/views/Front/footer.blade.php
@@ -4,8 +4,8 @@
     </div>
     <div class="right">
         <div class="images">
-            <img src="{{asset('img/logo-ut.png')}}" alt="Right Image 1">
-            <img src="{{asset('img/logo-ut.png')}}" alt="Right Image 2">
+            <img src="{{asset('img/move-black.png')}}" alt="Right Image 1" >
+            <img src="{{asset('img/ut-black.png')}}" alt="Right Image 2">
         </div>
         <div class="text">
             <div class="col-6 ms-5">
